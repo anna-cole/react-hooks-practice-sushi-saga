6459cee83f5ee6796b10434e7bb78ec64f97b866
Anna Raptidou Cole <acrrj123@gmail.com>
2023-10-18 17:34:33 -0500

    render 4 sushis at a time and more button working

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreButton from "./MoreButton";
 import Sushi from "./Sushi"
 
 function SushiContainer({ sushis }) {
+  const [sushiIndex, setSushiIndex] = useState(0)
+  
+  const sushiComponents = sushis
+  .slice(sushiIndex, sushiIndex + 4)
+  .map(sushi => <Sushi key={sushi.id} sushi={sushi} />);
+
+  function handleClickMore() {
+    // (sushiIndex + 4) % sushis.length
+    // is a way to wrap back around to the beginning of the array once we get to the end
+    // using the remainder (%) operator
+    setSushiIndex(sushiIndex => (sushiIndex + 4) % sushis.length);
+  }
+
   return (
     <div className="belt">
-      {sushis.map(sushi => 
-      <Sushi key={sushi.id} sushi={sushi}/>)}
-      <MoreButton />
+      {sushiComponents}
+      <MoreButton onClickMore={handleClickMore}/>
     </div>
   );
 }
